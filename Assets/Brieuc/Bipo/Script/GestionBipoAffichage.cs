30376f55b10e7c93b3373109463c679d89791ebf
TheSnipSnip <brieuctracol@gmail.com>
2023-10-25 12:52:54 +0200

    ajout compteur ressource
    ajout des compteur sur le tableau pour suivre l'augmentation des ressources

--- Assets/Brieuc/Bipo/Script/GestionBipoAffichage.cs
+++ Assets/Brieuc/Bipo/Script/GestionBipoAffichage.cs
@@ -11,10 +11,18 @@ public class GestionBipo : MonoBehaviour
     static public int nbreBipoGestion = 4;// variable 8 a changer par le nombre de bipo present dans le jeu(len(listedebipo))
     static public int bipoZzz = nbreBipoGestion-nbreBipoFarm-nbreBipoBucheron-nbreBipoMine;//nombre de bipo inactif
 
-    public Text scoreTextm;  
-    public Text scoreTextb;
-    public Text scoreTextf;
-    public Text scoreTextz;
+    static public int npierre = 0;
+    static public int nbois = 0;
+    static public int nouriturre = 0;
+
+    public Text Textm;  
+    public Text Textb;
+    public Text Textf;
+    public Text Textz;
+    // compteur de ressource
+    public Text stone;
+    public Text wood;
+    public Text food;
     void Start()
     {
 
@@ -22,9 +30,12 @@ public class GestionBipo : MonoBehaviour
 
     void Update() //affichage des nombres sur le tableau UI
     {
-        scoreTextm.text =" x " + nbreBipoMine.ToString();
-        scoreTextb.text =" x " + nbreBipoBucheron.ToString();
-        scoreTextf.text =" x " + nbreBipoFarm.ToString();
-        scoreTextz.text = " x " + bipoZzz.ToString();
+        Textm.text =" x " + nbreBipoMine.ToString();
+        Textb.text =" x " + nbreBipoBucheron.ToString();
+        Textf.text =" x " + nbreBipoFarm.ToString();
+        Textz.text = " x " + bipoZzz.ToString();
+        stone.text =npierre.ToString();
+        wood.text =nbois.ToString();
+        food.text =nouriturre.ToString();
     }
 }
